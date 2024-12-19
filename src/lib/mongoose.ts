0a3bcd7ded6ebdaa130dc799b34a47fefffcdd6c
CrewArth <105881935+CrewArth@users.noreply.github.com>
2024-12-19 13:56:09 +0530

    Uploaded /config, /lib, /context, /hooks

---
+++ src/lib/mongoose.ts
@@ -0,0 +1,47 @@
+import mongoose, { Connection } from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env');
+}
+
+interface MongooseCache {
+  conn: Connection | null;
+  promise: Promise<Connection> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
+
+export async function connectToDatabase(): Promise<Connection> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+      return mongoose.connection;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+} 
